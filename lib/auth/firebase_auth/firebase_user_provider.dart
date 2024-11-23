a3e8e2813b085ebddfae1493ffe9eb25b238aa62
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-23 08:36:36 +0000

    Updating to latest FlutterFlow output.

--- lib/auth/firebase_auth/firebase_user_provider.dart
+++ lib/auth/firebase_auth/firebase_user_provider.dart
@@ -32,6 +32,11 @@ class AastuStudentsFirebaseUser extends BaseAuthUser {
   }
 
   @override
+  Future? updatePassword(String newPassword) async {
+    await user?.updatePassword(newPassword);
+  }
+
+  @override
   Future? sendEmailVerification() => user?.sendEmailVerification();
 
   @override
